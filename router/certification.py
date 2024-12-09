7b8a57912377f47954226f09aba74b20be291785
pisceskibo <taquangtung2003@gmail.com>
2024-12-09 20:43:29 +0700

    rewrite information

--- router/certification.py
+++ router/certification.py
@@ -17,8 +17,8 @@ def write_certification():
         ### 2. Học bổng:
         + Đạt học bổng khuyến khích học tập kỳ 1 năm 2021 - 2022
         + Đạt học bổng khuyến khích học tập kỳ 3 năm 2022 - 2023
-        + Đạt học bổng Đỗ Quân năm 2023
-        + Đạt học bổng Chương trình trọng điểm quốc gia phát triển Toán học giai đoạn 2021 - 2030 kỳ 1
+        + Đạt học bổng Đỗ Quân năm học 2022 - 2023
+        + Đạt học bổng Chương trình trọng điểm quốc gia phát triển Toán học giai đoạn 2021 - 2030
         + Đạt học bổng khuyến khích học tập kỳ 6 năm 2023 - 2024
         + Đạt học bổng BIDV năm 2024
 
@@ -32,14 +32,16 @@ def write_certification():
 
         ### 4. Khen thưởng:
         + Giấy khen "Gương mặt trẻ tiêu biểu cấp cơ sở" năm 2022
+        + Giấy khen "PCN xuất sắc trong công tác Hội và phong trào sinh viên" năm 2022 - 2023
+        + Giấy khen "Sinh viên học tập tốt và tích cực trong công tác lớp" năm 2022 - 2023
         + Giấy khen "Sinh viên học tập tốt và tích cực trong công tác lớp" năm 2023 - 2024
-        + Giấy khen "PCN xuất sắc trong công tác Hội và phong trào sinh viên" năm 2023
         + Giấy khen "Thành viên tích cực trong CLB MIM Media" năm 2024
 
         ### 5. Giấy chứng nhận:
-        + Đạt GCN đội thi xuất sắc cuộc thi "Data Science FTU" năm 2022
+        + Đạt GCN đội thi tài năng của cuộc thi "Data Science FTU" năm 2022
         + Đạt GCN "Hành trình Sinh viên 5 Tốt - Bứt phá để thành công" năm 2022
         + Đạt GCN cuộc thi "Tự hào màu áo xanh" năm 2023
+        + Đạt GCN thí sinh xuất sắc cuộc thi "Simple Science 2023"
         + Đạt GCN cuộc thi "Con đường ánh sáng lần VII" năm 2024
         + Đạt GCN cuộc thi "Cùng hội rèn luyện để bản lĩnh hơn" năm 2024
         + Đạt GCN sinh viên xuất sắc lớp "Cảm tình Đảng" năm 2024
